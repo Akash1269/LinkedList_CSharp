03e21e58743a3eb5dc0ef99a3eb18828655dcc01
Akash Bhayekar <akashbhayekar@hotmail.com>
2017-05-14 08:59:15 +0530

    Add Functionalities like menu, diffrent ways of inserting elements in list, sort and search

--- LinkedList_CSharp/LinkedList.cs
+++ LinkedList_CSharp/LinkedList.cs
@@ -18,8 +18,175 @@ namespace LinkedList_CSharp
 
             head = node;
         }
+
+        public int DeleteAtStart()
+        {
+            int data = -1;
+            if (head != null)
+            {
+                data = head.Data;
+                head = head.Next;
+            }
+            return data;
+        }
+
+        public void InsertAtEnd(int data)
+        {
+            Node node = new Node();
+            Node temp = head;
+
+            node.Data = data;
+            if (head == null)
+            {
+                head = node;
+                return;
+            }
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+            temp.Next = node;
+        }
+
+        public int DeleteAtEnd()
+        {
+            int data = -1;
+            Node temp = head;
+
+            if (head == null)
+                return -1;
+            if (head.Next == null)
+            {
+                data = head.Data;
+                head = null;
+                return data;
+            }
+
+            while (temp.Next.Next != null)
+            {
+                temp = temp.Next;
+            }
+            data = temp.Data;
+            temp.Next = null;
+
+            return data;
+        }
+
+        public void InsertInSortedList(int data)
+        {
+            Node node = new Node() { Data = data, Next =  null};
+            
+            if(head == null || data <= head.Data)
+            {
+                node.Next = head;
+                head = node;
+                return;
+            }
+
+            Node temp = head;
+            while (temp.Next != null && temp.Next.Data < data)
+            {
+                temp = temp.Next;
+            }
+            node.Next = temp.Next;
+            temp.Next = node;
+        }
+
+        public int Find(int data)
+        {
+            int location = -1;
+            Node temp = head;
+            int i = 0;
+            while (temp != null)
+            {
+                i++;
+                if (temp.Data == data)
+                {
+                    location = i;
+                }
+                temp = temp.Next;
+            }
+            return location;
+        }
+
+        public void RemoveDuplicates()
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            HashSet<int> set = new HashSet<int>();
+
+            Node temp = head.Next;
+            Node tail = head;
+
+            set.Add(tail.Data);
+
+            while (temp != null)
+            {
+                if (set.Contains(temp.Data))
+                {
+                    tail.Next = temp.Next;
+                    temp = tail.Next;
+                }
+                else
+                {
+                    set.Add(temp.Data);
+                    tail = temp;
+                    temp = temp.Next;
+                }
+                
+            }
+        }
+
+        public void Reverse()
+        {
+            Node mid = null, tail = null;
+
+            while (head != null)
+            {
+                tail = mid;
+                mid = head;
+                head = head.Next;
+                mid.Next = tail;
+            }
+            head = mid;
+        }
+
+        void Swap(Node a, Node b)
+        {
+            int temp;
+            temp = a.Data;
+            a.Data = b.Data;
+            b.Data = temp;
+        }
+
+        public void Sort()
+        {
+            Node ptr;
+            bool sorted = false;
+
+            do
+            {
+                sorted = true;
+                ptr = head;
+                while (ptr.Next != null)
+                {
+                    if(ptr.Data > ptr.Next.Data)
+                    {
+                        sorted = false;
+                        Swap(ptr, ptr.Next);
+                    }
+                    ptr = ptr.Next;
+                }
+
+            } while (!sorted);
+        }
+
         public void PrintList()
         {
+            Console.WriteLine("\nPlease find below the linked list - ");
             Node temp = head;
             if (temp == null)
             {
@@ -27,10 +194,12 @@ namespace LinkedList_CSharp
             }
             while (temp != null)
             {
-                Console.Write(temp.Data + " ");
+                Console.Write(temp.Data + " -> ");
                 temp = temp.Next;
             }
+            Console.Write("null\n");
         }
+
         public int GetSize()
         {
             Node temp = head;
@@ -41,7 +210,12 @@ namespace LinkedList_CSharp
                 count++;
             }
             return count;
-        }        
+        }
+
+        public void EmptyList()
+        {
+            head = null;
+        }
     }
 
 }
--- LinkedList_CSharp/Program.cs
+++ LinkedList_CSharp/Program.cs
@@ -11,7 +11,7 @@ namespace LinkedList_CSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked Program");
-            Console.WriteLine("\n\nPlease enter numbers to be inserted into the list in space separated format");
+            Console.WriteLine("\nPlease enter numbers to be inserted into the list in space separated format");
 
             string[] temp = Console.ReadLine().Split(' ');
             int[] values = Array.ConvertAll(temp, int.Parse);
@@ -22,9 +22,90 @@ namespace LinkedList_CSharp
             {
                 list.InsertAtStart(item);
             }
-            list.PrintList();
-            
-            Console.Read();
+            int option = 1;
+            int d;
+            while (option != 0)
+            {
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("Exiting!!!");
+                        break;
+                    case 1:
+                        list.PrintList();
+                        break;
+                    case 2:
+                        Console.WriteLine(list.GetSize());
+                        break;
+                    case 3:
+                        list.EmptyList();
+                        Console.WriteLine("List deleted successfully");
+                        break;
+                    case 4:
+                        Console.WriteLine("Please enter the intger value for the element to be inserted : ");
+                        d = int.Parse(Console.ReadLine());
+                        list.InsertAtStart(d);
+                        break;
+                    case 5:
+                        list.DeleteAtStart();
+                        Console.WriteLine("Element at the start of the list deleted successfully");
+                        break;
+                    case 6:
+                        Console.WriteLine("Please enter the intger value for the element to be inserted : ");
+                        d = int.Parse(Console.ReadLine());
+                        list.InsertAtEnd(d);
+                        break;
+                    case 7:
+                        list.DeleteAtEnd();
+                        Console.WriteLine("Element at the end of the list deleted successfully");
+                        break;
+                    case 8:
+                        Console.WriteLine("Please enter the the value to be searched");
+                        d = int.Parse(Console.ReadLine());
+                        Console.WriteLine("The element present at address - " + list.Find(d));
+                        break;
+                    case 9:
+                        list.RemoveDuplicates();
+                        Console.WriteLine("Remvoed duplicates successfully");
+                        break;
+                    case 10:
+                        list.Reverse();
+                        Console.WriteLine("List has been reversed successfully");
+                        break;
+                    case 11:
+                        list.Sort();
+                        Console.WriteLine("List has been sorted successfully");
+                        break;
+                    case 12:
+                        d = int.Parse(Console.ReadLine());
+                        list.InsertInSortedList(d);
+                        Console.WriteLine("element inserted successfully");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid options, Please enter the Correct choice Again");
+                        break;
+                }
+                list.PrintList();
+
+                Console.WriteLine(@"
+                    Plese select option from the menu
+                    0.Exit
+                    1.Print List
+                    2.Get Size
+                    3.Delete List
+                    4.Insert At Start
+                    5.Delete At Start
+                    6.Insert At End
+                    7.Delete At End
+                    8.Find
+                    9.Remove Duplicates
+                    10.Reverse
+                    11.Sort
+                    12.Insert in sorted array (please sort before inserting)
+                    ");
+
+                option = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
